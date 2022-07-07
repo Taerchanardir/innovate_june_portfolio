12a9e591c54fc9705f70ab83c98fcea16f70ce7b
Iaerchanardir <86472165+Taerchanardir@users.noreply.github.com>
2022-07-07 10:29:13 +0100

    Added some comments

--- backend_project/views.py
+++ backend_project/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for
 my_view = Blueprint('my_view', __name__)
 
+# return the site urls to the appropriate pages
 @my_view.route('/')
 def index():
   return render_template("index.html")
@@ -25,6 +26,7 @@ def page4():
 def page5():
   return render_template("page5.html")
 
+# redirect a few pages the user should not have access to back to home page
 @my_view.route('/js')
 @my_view.route('/javascript')
 @my_view.route('/home')
@@ -33,6 +35,7 @@ def general_redirects():
   return redirect(url_for("my_view.index"))
   # Second one is better, it also changes the url in the browser navbar
 
+# 'secret' admin page, without an entry in the navbar. It doesn't have a user/password, but it doesn't have any content either
 @my_view.route('/admin')
 def admin_page():
   return render_template("admin.html")
